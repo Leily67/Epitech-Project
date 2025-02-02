d1348848b1400e2c809bd48bc38ec760b1b2c4f0
colette <colette.oswald@epitech.eu>
2025-02-02 20:36:14 +0100

    feat: add the base and final code for the naval battle game

---
+++ Web@cademie/First_Year/Battleship/step05_ex03/player.js
@@ -0,0 +1,116 @@
+/*jslint browser this */
+/*global _, shipFactory, player, utils */
+
+(function (global) {
+    "use strict";
+
+    var ship = {dom: {parentNode: {removeChild: function () {}}}};
+
+    var player = {
+        grid: [],
+        tries: [],
+        fleet: [],
+        game: null,
+        activeShip: 0,
+        init: function () {
+            // créé la flotte
+            this.fleet.push(shipFactory.build(shipFactory.TYPE_BATTLESHIP));
+            this.fleet.push(shipFactory.build(shipFactory.TYPE_DESTROYER));
+            this.fleet.push(shipFactory.build(shipFactory.TYPE_SUBMARINE));
+            this.fleet.push(shipFactory.build(shipFactory.TYPE_SMALL_SHIP));
+
+            // créé les grilles
+            this.grid = utils.createGrid(10, 10);
+            this.tries = utils.createGrid(10, 10);
+        },
+
+        // Etape 00 : Function SetGame - Mise en place du jeu
+        setGame: function (game) {
+            this.game = game;
+        },
+        play: function (col, line) {
+            // appel la fonction fire du game, et lui passe une calback pour récupérer le résultat du tir
+            this.game.fire(this, col, line, _.bind(function (hasSucced) {
+                this.tries[line][col] = hasSucced;
+            }, this));
+        },
+        // quand il est attaqué le joueur doit dire si il a un bateaux ou non à l'emplacement choisi par l'adversaire
+        receiveAttack: function (col, line, callback) {
+            var succeed = false;
+
+            if (this.grid[line][col] !== 0) {
+                succeed = true;
+                this.grid[line][col] = 0;
+            }
+            callback.call(undefined, succeed);
+        },
+
+        //etape 3 : colision
+        setActiveShipPosition: function (x, y, axe) {
+            var ship = this.fleet[this.activeShip];
+            var axe = this.game.axe;
+
+            var i = 0;
+            var j = 0;
+            var h = 0;
+
+            if(this.game.axe === "Horizontale"){
+                x = x - Math.floor(ship.getLife()/2);
+                while(j< ship.getLife()){
+                    if(this.grid[y][x + j] != 0){
+                        return false;
+                    }
+                }
+            }
+
+            while (i < ship.getLife()) {
+                this.grid[y][x + i] = ship.getId();
+                i += 1;
+            }
+
+            return true;
+        },
+
+        //etape 01 : Effacement des bateaux au click ok - 'Sheep' remplacer par ship
+        clearPreview: function () {
+            this.fleet.forEach(function (ship) {
+                if (ship.dom.parentNode) {
+                    ship.dom.parentNode.removeChild(ship.dom);
+                }
+            });
+        },
+        resetShipPlacement: function () {
+            this.clearPreview();
+
+            this.activeShip = 0;
+            this.grid = utils.createGrid(10, 10);
+        },
+        activateNextShip: function () {
+            if (this.activeShip < this.fleet.length - 1) {
+                this.activeShip += 1;
+                return true;
+            } else {
+                return false;
+            }
+        },
+        renderTries: function (grid) {
+            this.tries.forEach(function (row, rid) {
+                row.forEach(function (val, col) {
+                    var node = grid.querySelector('.row:nth-child(' + (rid + 1) + ') .cell:nth-child(' + (col + 1) + ')');
+
+                    if (val === true) {
+                        node.style.backgroundColor = '#e60019';
+                    } else if (val === false) {
+                        node.style.backgroundColor = '#aeaeae';
+                    }
+                });
+            });
+        },
+        renderShips: function (grid) {
+        }
+    };
+
+    global.player = player;
+    // console.log(player);
+
+}(this));
